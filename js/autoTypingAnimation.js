b005e57c9542effa4d122c459e9c21c8130fedcd
saurabh <saurabh10032001.com>
2023-04-04 00:40:14 +0530

    first commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi,<br> I am Saurabh Yadav</span> <br>B-Tech CSE Final Year Student <br><br><br><br><br><br><br><br><br>"
+      ],
+      typeSpeed: 100,
+      contentType: 'html'
+    });
+  }, 100);
