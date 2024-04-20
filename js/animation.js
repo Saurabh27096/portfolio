405107f9356b2a4685d6ee5cefeb042b476a85e3
Saurabh Yadav <78016238+Saurabh27096@users.noreply.github.com>
2024-04-20 14:09:04 +0530

    Update animation.js added spring boot developer

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Saurabh Yadav","Problem solver", "Flutter Developer"];
+const textArray = ["Saurabh Yadav","Spring Boot Developer", "Problem solver", "Flutter Developer"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
